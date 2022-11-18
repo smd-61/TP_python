5f01b21cc199af9109588f0d539c5dd7569216d2
Samed Koc <samed.koc@uha.fr>
2022-11-18 15:27:04 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("choisissez la valeur de x")
+x=int(input())
+print("choisissez la valeur de y")
+y=int(input())
+tmp=x
+x=y
+y=tmp
+print("Les valeurs ont été permuter")
