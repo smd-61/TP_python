336f11ec1c43e11519df11c0bc16f525a3853fd6
smd-61 <samedk6767@gmail.com>
2023-01-13 09:48:15 +0100

    mise en forme

--- tp4.py
+++ tp4.py
@@ -99,3 +99,119 @@ for i in range(0, len(L1)):
             b=L1[i]
 print(f"Le nombre le plus frequent dans la liste est le :{b} ({a}x)")
 
+#exercice 5
+q=0
+a=0
+b=0
+c=0
+z=0
+y=0
+u=0
+r=0
+R=[]
+T=input("Donner la date")
+if len(T)==8:
+    for i in range (0,8):
+        R.append(int(T[i]))
+    a=R[0]*10+R[1]
+    b=R[2]*10+R[3]
+    c=R[4]*1000+R[5]*100+R[6]*10+R[7]
+
+    if b>12 or b<=0:
+        print(f"La date {T[0]}{T[1]}/{T[2]}{T[3]}/{T[4]}{T[5]}{T[6]}{T[7]} est incorrecte")
+    elif b==(1 or 3 or 5 or 7 or 8 or 10 or 12):
+        z=1
+        r = 1
+    else :
+        z=2
+        r = 1
+    if  (c%4==0 and c%100!=0) or c%400==0:
+        y=4
+
+
+    if b==2:
+        if y==4:
+            if a<=0 or a>29:
+                print(f"La date {T[0]}{T[1]}/{T[2]}{T[3]}/{T[4]}{T[5]}{T[6]}{T[7]} est incorrecte")
+        else:
+            if a<= 0 or a > 28:
+                print(f"La date {T[0]}{T[1]}/{T[2]}{T[3]}/{T[4]}{T[5]}{T[6]}{T[7]} est incorrecte")
+            else:
+                u=5
+    else:
+        if z==1:
+            if a<=0 or a>31:\
+                print(f"La date {T[0]}{T[1]}/{T[2]}{T[3]}/{T[4]}{T[5]}{T[6]}{T[7]} est incorrecte")
+        else:
+            if a<=0 or a>30:
+                print(f"La date {T[0]}{T[1]}/{T[2]}{T[3]}/{T[4]}{T[5]}{T[6]}{T[7]} est incorrecte")
+            else:
+                u=5
+    if u==5 and r==1:
+        print("La date est correcte")
+else:
+    print("La date es incorrecte")
+
+#exercice6
+T=[5, 2, 4, 8, 1, 3]
+a=0
+print(T)
+for i in range (0,len(T)):
+    a, z = T[i], i
+    for j in range (i+1,len(T)):
+        if T[j]<a:
+            a=T[j]
+            z=j
+
+    T[i],T[z]=T[z],T[i]
+    print(T)
+
+T=[5, 2, 4, 8, 1, 3]
+
+print(sorted(T))
+T.sort()
+print(T)
+
+#exercice7
+a=input("Entrer votre login")
+b=input("Entrer celui de votre voisin")
+binome=(a,b)
+print(f"L’étudiant {a} est en binome avec l’étudiant {b}")
+
+#exercice8
+dico = {}
+dico['nom'] = 'toto'
+dico['prénom'] = 'titi'
+dico['promo'] = '2023'
+dico['groupe'] = '202'
+
+dic = {}
+dic['nom'] = 'tata'
+dic['prénom'] = 'tutu'
+dic['promo'] = '2023'
+dic['groupe'] = '102'
+
+binome = {}
+binome['élève1']= dico
+binome['élève2']= dic
+
+
+print(f"Votre nom est {dico['nom']}, votre prénom est {dico['prénom']}, vous faites partie de la promo {dico['promo']} et votre groupe est {dico['groupe']}")
+print("Les clés du dictionnaire sont :")
+for cle, valeur in dico.items():
+    print("-",cle)
+
+print("Les valeurs du dictionnaire sont :")
+for cle, valeur in dico.items():
+    print("-",valeur)
+
+print("Les tuplets du dictionnaire sont :")
+for valeur in dico.items():
+    print("-",valeur)
+
+
+print("Les étudiants formants le binôme sont :")
+
+
+print(f"- L'étudiant {binome['élève1']['nom']} {binome['élève1']['prénom']} du groupe {binome['élève1']['groupe']}")
+print(f"- L'étudiant {binome['élève2']['nom']} {binome['élève2']['prénom']} du groupe {binome['élève2']['groupe']}")
